e4e71a9916c53605778a211151ff7cc4077773f9
sogud007 <andres.salazar@javerianacali.edu.co>
2021-02-23 09:33:51 -0500

    configuración hibernate

--- inventory/src/main/java/com/javeriana/web/inventory/controllers/UnitMeasurementController.java
+++ inventory/src/main/java/com/javeriana/web/inventory/controllers/UnitMeasurementController.java
@@ -1,9 +1,19 @@
 package com.javeriana.web.inventory.controllers;
 
+import com.javeriana.web.inventory.models.UnitMeasurement;
 import com.javeriana.web.inventory.services.UnitMeasurementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.javeriana.web.inventory.InventoryApplication.LOGGER;
 
 @RestController
 @RequestMapping(value = "/unit")
@@ -16,6 +26,19 @@ public class UnitMeasurementController {
         this.unitMeasurementService = unitMeasurementService;
     }
 
-
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<UnitMeasurement>> getUnits() {
+        List<UnitMeasurement> units = null;
+        HttpStatus codigo = HttpStatus.FORBIDDEN;
+        try {
+            units = this.unitMeasurementService.getAllUnitMeasurement();
+            codigo = HttpStatus.OK;
+        }
+        catch (Exception e) {
+            LOGGER.error("UnitMeasurementController.getUnits Cause: " + e.getMessage());
+        }
+        LOGGER.error("Mensaje de Error de Prueba.");
+        return new ResponseEntity<List<UnitMeasurement>>(units, codigo);
+    }
 
 }
